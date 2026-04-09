9e9d537a47bc4eff2fffab79d2a4b2a9db553f9d
Gningue Mamadou <91191658+YRNMAMA@users.noreply.github.com>
2026-04-09 14:16:40 +0000

    Aggiungi configurazione iniziale per l'applicazione Videogiochi con Maven, inclusi file di configurazione, classi principali e dipendenze

---
+++ videogiochi/src/main/java/itt/marconi/videogiochi/VideogiochiApplication.java
@@ -0,0 +1,13 @@
+package itt.marconi.videogiochi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideogiochiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideogiochiApplication.class, args);
+	}
+
+}
---
+++ videogiochi/src/test/java/itt/marconi/videogiochi/VideogiochiApplicationTests.java
@@ -0,0 +1,13 @@
+package itt.marconi.videogiochi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideogiochiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
